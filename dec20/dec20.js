056f2dcfe6394b5575ca5ff29b7b92cfcb1329a3
Jared Lindquist <jared.lindquist1985@gmail.com>
2022-12-20 10:45:23 -0600

    2 kata solved

---
+++ dec20/dec20.js
@@ -0,0 +1,20 @@
+
+function makeNegative(num) {
+    return -Math.abs(num);
+}
+console.log(makeNegative(33)); //-33
+console.log(makeNegative(-12)); // -12
+console.log(makeNegative(345));  // -345
+console.log(makeNegative(9009)); //-9009
+console.log(makeNegative(-9347)); //-9347
+
+//------------------------
+
+function booleanToString(b){
+    //your code here
+    return (b).toString();
+}
+
+console.log(booleanToString(true));
+console.log(booleanToString(false));
+
