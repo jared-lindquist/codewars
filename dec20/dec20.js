9f9fc2ede97e8bcecc7e6021225a4010ebf55d74
Jared Lindquist <jared.lindquist1985@gmail.com>
2022-12-26 11:33:40 -0600

    #10

--- dec20/dec20.js
+++ dec20/dec20.js
@@ -116,3 +116,16 @@ console.log(stringToArray('this is fun'));
 console.log(stringToArray('hello world'));
 console.log(stringToArray('merry christmas'));
 //---------------------------------------
+//10.
+function lovefunc(flower1, flower2){
+    if (flower1 % 2 === 0 && flower2 % 2 === 1 || flower1 % 2 === 1 && flower2 % 2 === 0) {
+    return true;
+    } else {
+    return false;
+    }
+}
+
+console.log(lovefunc(2, 5));
+console.log(lovefunc(4, 24));
+console.log(lovefunc(567, 23));
+console.log(lovefunc(488, 35));
