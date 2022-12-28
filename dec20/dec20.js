788ba5573ba71ad92c9afb42939c9119c6b4e0d9
Jared Lindquist <jared.lindquist1985@gmail.com>
2022-12-28 10:04:57 -0600

    #12

--- dec20/dec20.js
+++ dec20/dec20.js
@@ -139,3 +139,19 @@ return String(n).split("").reverse().map(Number);
 console.log(digitize(78634));
 console.log(digitize(098237));
 console.log(digitize(759246));
+//----------------
+//12.
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  // TODO
+  if (distanceToPump <= (mpg * fuelLeft)) {
+    return true
+    } else {
+    return false;
+    }
+};
+
+console.log(zeroFuel(75,25,3));
+console.log(zeroFuel(100,25,3));
+console.log(zeroFuel(400,50,8));
+//--------------------
+//13.
